68042b34f202713fc514da7e22e1c4129759ad5c
Timofei Istomin <tim.ist@gmail.com>
2020-02-11 11:25:56 +0100

    Add a "soft assertion" feature

--- core/sys/print-def.h
+++ core/sys/print-def.h
@@ -13,6 +13,20 @@
 #define _STRDEF_STR(x) #x
 #define STRDEF(x) (#x ": " _STRDEF_STR(x))
 
+/* "Soft" assertions.
+ *
+ * Use WARNIF(condition) to print a warning if the condition holds
+ *
+ * Enable it in a per-file basis by defining ENABLE_WARNIF prior to
+ * including this header file.
+ */
+
+#ifdef ENABLE_WARNIF
+#define WARNIF(x) do {if (x) printf("Check (" #x ") failed in %s (%s:%d)\n", __func__, __FILE__, __LINE__);} while(0)
+#else
+#define WARNIF(x) do {} while(0)
+#endif
+
 #endif // PRINT_DEF_H
 
 
