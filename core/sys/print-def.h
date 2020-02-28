b1a9377ba051d447fcec6882b204081ba1672f62
Timofei Istomin <tim.ist@gmail.com>
2020-02-28 01:08:48 +0100

    Add a simple but mighty logging facility (draft)

--- core/sys/print-def.h
+++ core/sys/print-def.h
@@ -1,31 +1,55 @@
 #ifndef PRINT_DEF_H
 #define PRINT_DEF_H
 
-/*
-    Print a MACRO at compile time!
-    Use a pragma directive as following:
+/* Print a MACRO at compile time!
+   Use a pragma directive as following:
 
-    #pragma message STRDEF(YOUR_MACRO)
-
+   #pragma message STRDEF(YOUR_MACRO)
 */
-
 
 #define _STRDEF_STR(x) #x
 #define STRDEF(x) (#x ": " _STRDEF_STR(x))
 
-/* "Soft" assertions.
+/* Logging-related defines
  *
- * Use WARNIF(condition) to print a warning if the condition holds
- *
- * Enable it in a per-file basis by defining ENABLE_WARNIF prior to
- * including this header file.
+ * You may set LOG_PREFIX and LOG_LEVEL on a file-by-file basis.
+ * Make sure to set them before including this header file.
  */
 
-#ifdef ENABLE_WARNIF
-#define WARNIF(x) do {if (x) printf("Check (" #x ") failed in %s (%s:%d)\n", __func__, __FILE__, __LINE__);} while(0)
-#else
-#define WARNIF(x) do {} while(0)
+#define LOG_NONE 1          // no printing
+#define LOG_ERR  2          // critical errors (cannot proceed)
+#define LOG_WARN 3          // warinings (proceed but may fail)
+#define LOG_DBG  4          // debug messages
+#define LOG_INFO 5          // less important info
+#define LOG_ALL LOG_INFO    // print it all
+
+#ifndef LOG_PREFIX
+#define LOG_PREFIX __FILE__ // file name by default
 #endif
+
+
+#if defined(LOG_LEVEL) && (LOG_LEVEL < LOG_NONE || LOG_LEVEL > LOG_ALL)
+#error Log level set incorrectly: LOG_LEVEL
+#endif
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_WARN  // warnings by default
+#endif
+
+
+// TODO: actually follow the log level
+
+#include <stdio.h>
+
+#define ERR(format, ...) do {printf("["LOG_PREFIX"] ERR:" format "\n" __VA_OPT__(,) __VA_ARGS__);} while(0)
+#define WARN(format, ...) do {printf("["LOG_PREFIX"] WARN:" format "\n" __VA_OPT__(,) __VA_ARGS__);} while(0)
+#define DBG(format, ...) do {printf("["LOG_PREFIX"] DBG:" format "\n" __VA_OPT__(,) __VA_ARGS__);} while(0)
+
+
+/* "Soft" assertions.
+ * Use WARNIF(condition) to print a warning if the condition holds
+ */
+#define WARNIF(x) do {if (x) WARN("Check (" #x ") failed in %s (%s:%d)\n", __func__, __FILE__, __LINE__);} while(0)
 
 #endif // PRINT_DEF_H
 
