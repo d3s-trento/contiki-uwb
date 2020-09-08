141a0a4977cc49b29d8e8be68550a0b0f8f7149a
Timofei Istomin <tim.ist@gmail.com>
2020-09-08 16:30:55 +0200

    ranging example: change to 2-byte address

--- examples/ranging/project-conf.h
+++ examples/ranging/project-conf.h
@@ -1,8 +1,9 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-//#define LINKADDR_CONF_SIZE 8
 #define LINKADDR_CONF_SIZE 2
+//#define LINKADDR_CONF_SIZE 8
+//#define DWM1001_USE_BT_ADDR_FOR_UWB 1
 
 #define APP_RADIO_CONF 1
 
