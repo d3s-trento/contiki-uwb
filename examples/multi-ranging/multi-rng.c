cb9aaaa3ace91a0f30f119dbdcf33196ade9a66e
Timofei Istomin <tim.ist@gmail.com>
2021-03-22 21:09:37 +0100

    Insert more delays and checks for safety

--- examples/multi-ranging/multi-rng.c
+++ examples/multi-ranging/multi-rng.c
@@ -48,7 +48,7 @@
 #include "dw1000-config.h"
 #include "core/net/linkaddr.h"
 /*--------------------------------------------------------------------------*/
-#define DEBUG 0
+#define DEBUG 1
 #if DEBUG
 #define PRINTF(...) printf(__VA_ARGS__)
 #else
@@ -56,46 +56,47 @@
 #endif
 
 /*-- Configuration ----------------------------------------------------------*/
-linkaddr_t master_tag = {{0x13, 0x9a}}; // orchestrates other tags
+linkaddr_t master_tag = {{0x18, 0x32}}; // 11 // orchestrates other tags
 linkaddr_t other_tags[] = { // tags, wait for the schedule from the master (may be empty)
-  {{0x19, 0x15}},
-  {{0x11, 0xa3}},
-  {{0x11, 0x0c}},
-  {{0x12, 0x8a}},
-  {{0x10, 0x9b}},
-  {{0x18, 0x33}},
 }; 
 linkaddr_t anchors[] = { // responders (a node can be both a tag and an anchor)
-  {{0x13, 0x9a}},
-  {{0x19, 0x15}},
-  {{0x11, 0xa3}},
-  {{0x11, 0x0c}},
-  {{0x12, 0x8a}},
-  {{0x10, 0x9b}},
-  {{0x18, 0x33}},
+  {{0x10, 0x89}}, // 8
+  {{0x11, 0x4a}}, // 9
+  {{0xc8, 0x0c}}, // 10
+  {{0x18, 0x32}}, // 11
+  {{0x17, 0x93}}, // 12
+  {{0x02, 0xd8}}, // 13
+  {{0xca, 0x99}}, // 14
+  {{0x14, 0x53}}, // 15
+  {{0x51, 0x08}}, // 16
+  {{0x15, 0x81}}, // 17
+  //{{0xaa, 0xaa}}, // invalid node for debugging
 };
-#define RANGING_STYLE     DW1000_RNG_SS   // single- or double-sided (DW1000_RNG_DS)
-#define RANGING_INTERVAL (CLOCK_SECOND*2)   // period of multi-ranging
+#define RANGING_STYLE  DW1000_RNG_SS     // single- or double-sided (DW1000_RNG_DS)
+#define ROUND_PERIOD   (CLOCK_SECOND*1)  // period of multi-ranging
 
-#define ACQUIRE_CIR 0           // 1 = enable CIR acquisition
+#define ACQUIRE_CIR 1           // 1 = enable CIR acquisition
 #define CIR_START_FROM_PEAK 0   // 0 = print from beginning, 1 = print starting from the first ray peak
 #define CIR_MAX_SAMPLES DW1000_CIR_MAX_LEN // number of CIR samples to acquire
-#define PRINT_RXDIAG 0          // 1 = enable printing RX diagnostics
+#define PRINT_RXDIAG 1          // 1 = enable printing RX diagnostics
 
 /*--------------------------------------------------------------------------*/
 #if ACQUIRE_CIR
-#define MAX_PRINTING_DELAY (CLOCK_SECOND / 44)    // time needed to print a full CIR (upper bound)
+#define MAX_PRINTING_TIME (CLOCK_SECOND / 20)     // estimated time needed to print a full CIR (depends on the platform)
+#define CIR_DUMP_DELAY    (CLOCK_SECOND / 200)    // a delay inserted after printing CIR to let the USB buffer get emptied
 #else
-#define MAX_PRINTING_DELAY (CLOCK_SECOND / 1000)  // time needed to print ranging results and diagnostics (upper bound)
+#define MAX_PRINTING_TIME (CLOCK_SECOND / 1000)   // time needed to print ranging results and diagnostics (upper bound)
+#define CIR_DUMP_DELAY 0
 #endif
 /*--------------------------------------------------------------------------*/
 #define INIT_GUARD 2 // leave one-two ticks between the command tx/rx and the first ranging slot
+#define RANGING_GAP 2 // leave one-two ticks between consecutive rangings
 #if RANGING_STYLE == DW1000_RNG_SS
 #define RANGING_TIME (CLOCK_SECOND / 1000)  // time allocated for a single TWR
 #else
 #define RANGING_TIME (CLOCK_SECOND / 500)
 #endif
-#define TOTAL_RANGING_TIME (RANGING_TIME + MAX_PRINTING_DELAY)
+#define TOTAL_RANGING_TIME (RANGING_TIME + MAX_PRINTING_TIME + CIR_DUMP_DELAY + RANGING_GAP)
 #define NUM_OTHER_TAGS (sizeof(other_tags) / sizeof(other_tags[0]))
 #define NUM_ANCHORS (sizeof(anchors) / sizeof(anchors[0]))
 
@@ -103,7 +104,7 @@ linkaddr_t anchors[] = { // responders (a node can be both a tag and an anchor)
 #define TOTAL_ROUND_DURATION (TAG_SLOT_DURATION*(NUM_OTHER_TAGS+1) + INIT_GUARD)
 
 // sanity check that there is enough time for ranging
-_Static_assert (RANGING_INTERVAL > TOTAL_ROUND_DURATION + CLOCK_SECOND/100, 
+_Static_assert (ROUND_PERIOD > TOTAL_ROUND_DURATION + CLOCK_SECOND/100, 
                 "Not enough time for ranging");
 
 /*--------------------------------------------------------------------------*/
@@ -138,6 +139,7 @@ AUTOSTART_PROCESSES(&ranging_process);
 /*--------------------------------------------------------------------------*/
 static process_event_t master_control_event;
 static uint32_t seqn;
+static bool busy_ranging;
 static uint8_t tag_slot;
 /*--------------------------------------------------------------------------*/
 static void bc_recv(struct broadcast_conn *c, const linkaddr_t *from);
@@ -170,7 +172,7 @@ PROCESS_THREAD(ranging_process, ev, data)
   role = ROLE_ANCHOR;
   if(linkaddr_cmp(&linkaddr_node_addr, &master_tag)) {
     broadcast_open(&broadcast, 129, &broadcast_call);
-    etimer_set(&et, RANGING_INTERVAL);
+    etimer_set(&et, ROUND_PERIOD);
     printf("Role: master_tag\n");
     role = ROLE_TAG_MASTER;
   }
@@ -188,103 +190,120 @@ PROCESS_THREAD(ranging_process, ev, data)
     }
   }
 
-  /* Ranging loop for tags */
-  while(1) {
+  if (role == ROLE_TAG_MASTER || role == ROLE_TAG) {
+    /* Ranging loop for tags */
+    while(1) {
 
-    /* The master tag sends a control message embedding all
-     * tags in the order they must use to perform ranging;
-     * other tags wait for the control message */
-    if(role == ROLE_TAG_MASTER) {
-      PROCESS_YIELD_UNTIL(etimer_expired(&et));
-      etimer_set(&et, RANGING_INTERVAL);
-      if (NUM_OTHER_TAGS > 0) { // only send the command if there are other tags
-        PRINTF("Sending control %lu\n", seqn);
-        bool res = fill_ctrl_packet();
-        if (res) {
-          broadcast_send(&broadcast);
+      /* The master tag sends a control message embedding all
+       * tags in the order they must use to perform ranging;
+       * other tags wait for the control message */
+      if(role == ROLE_TAG_MASTER) {
+        PROCESS_YIELD_UNTIL(etimer_expired(&et));
+        etimer_set(&et, ROUND_PERIOD);
+        if (NUM_OTHER_TAGS > 0) { // only send the command if there are other tags
+          PRINTF("Sending control %lu\n", seqn);
+          bool res = fill_ctrl_packet();
+          if (res) {
+            broadcast_send(&broadcast);
+          }
+          // wait for the broadcast to complete
+          PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_POLL);
         }
-        // wait for the broadcast to complete
-        PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_POLL);
+        tag_slot = 0; // master always has slot 0
+        seqn++;
       }
-      tag_slot = 0; // master always has slot 0
-      seqn++;
-    }
-    else if(role == ROLE_TAG) {
-      PROCESS_YIELD_UNTIL(ev == master_control_event);
-      // tag_slot was filled according to the received packet
-    }
-    else break;
+      else if(role == ROLE_TAG) {
+        PROCESS_YIELD_UNTIL(ev == master_control_event);
+        // tag_slot was filled according to the received packet
+      }
 
-    /* Compute waiting time to let other tags do ranging */
-    clock_time_t tag_wait = INIT_GUARD + tag_slot * TAG_SLOT_DURATION;
-    if(tag_wait != 0) {
-      etimer_set(&et_slot, tag_wait);
-      PROCESS_WAIT_UNTIL(etimer_expired(&et_slot));
-    }
+      busy_ranging = 1; // mark that we're busy till the end of the round
+      PRINTF("Prepare ranging [%lu] (slot: %u)\n", seqn, tag_slot);
 
-    static clock_time_t slot_start;
-    slot_start = clock_time();
+      /* Compute waiting time to let other tags do ranging */
+      clock_time_t tag_wait = INIT_GUARD + tag_slot * TAG_SLOT_DURATION;
+      if(tag_wait != 0) {
+        etimer_set(&et_slot, tag_wait);
+        PROCESS_WAIT_UNTIL(etimer_expired(&et_slot));
+      }
 
-    /* Range with each anchor */
-    for(i=0; i<NUM_ANCHORS; i++) {
-      static linkaddr_t dst;
-      dst = anchors[i];
-      if(linkaddr_cmp(&linkaddr_node_addr, &dst)) continue;
-      printf("RNG [%lu/%lums] %02x%02x->%02x%02x: ",
-        seqn, (clock_time() * 1000UL / CLOCK_SECOND),
-        linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
-        dst.u8[0], dst.u8[1]);
+      static clock_time_t slot_start;
+      slot_start = clock_time();
+
+      /* Range with each anchor */
+      for(i=0; i<NUM_ANCHORS; i++) {
+        static linkaddr_t dst;
+        dst = anchors[i];
+        if(linkaddr_cmp(&linkaddr_node_addr, &dst)) continue;
+        printf("RNG [%lu/%lums] %02x%02x->%02x%02x: ",
+            seqn, (clock_time() * 1000UL / CLOCK_SECOND),
+            linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
+            dst.u8[0], dst.u8[1]);
 #if ACQUIRE_CIR
-      dw1000_ranging_acquire_diagnostics(
-                                 CIR_START_FROM_PEAK ? DW1000_GET_CIR_FROM_FP : 0,
-                                 CIR_MAX_SAMPLES,
-                                 cir_buf);
+        dw1000_ranging_acquire_diagnostics(
+            CIR_START_FROM_PEAK ? DW1000_GET_CIR_FROM_FP : 0,
+            CIR_MAX_SAMPLES,
+            cir_buf);
 #else // only request diagnostics
-      dw1000_ranging_acquire_diagnostics(0, 0, NULL);
+        dw1000_ranging_acquire_diagnostics(0, 0, NULL);
 #endif
-      status = range_with(&dst, RANGING_STYLE);
-      if(!status) {
-        printf("REQ FAIL\n");
-      }
-      else {
-        PROCESS_YIELD_UNTIL(ev == ranging_event);
-        if(((ranging_data_t *)data)->status) {
-          ranging_data_t *d = data;
-          dw1000_rxpwr_t rxpwr;
-          dw1000_rxpwr(&rxpwr, &d->rxdiag, dw1000_get_current_cfg());
-          printf("SUCCESS %d bias %d fppwr %d rxpwr %d cifo %d\n",
-            (int)(100*d->raw_distance), (int)(100*d->distance),
-            (int)(1000*rxpwr.fp_pwr), (int)(1000*rxpwr.rx_pwr),
-            (int)(10e8*d->freq_offset));
-#if ACQUIRE_CIR
-          uint16_t cir_start = cir_buf[0];
-          uint16_t cir_fp_int = d->rxdiag.firstPath >> 6;
-          uint16_t cir_fp_frac = d->rxdiag.firstPath & 0x3f;
-          // fp (first path) is printed as the integer part and the 1/64 fractional part
-          // "fp a#b" means (a + b/64)
-          printf("CIR [%lu] %02x%02x->%02x%02x %d#%d [%d:%d] ", 
-              seqn,
-              linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
-              dst.u8[0], dst.u8[1],
-              cir_fp_int, cir_fp_frac, cir_start, d->cir_samples_acquired);
-          dw1000_print_cir_hex(cir_buf+1, d->cir_samples_acquired);
-#endif
-#if PRINT_RXDIAG
-          printf("DIAG [%lu] %02x%02x->%02x%02x ", 
-          seqn,
-          linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
-          dst.u8[0], dst.u8[1]);
-          print_rxdiag(&d->rxdiag, &rxpwr);
-#endif
+        status = range_with(&dst, RANGING_STYLE);
+        if(!status) {
+          printf("REQ FAIL\n");
         }
         else {
-          printf("FAIL\n");
+          PROCESS_YIELD_UNTIL(ev == ranging_event);
+          if(((ranging_data_t *)data)->status) {
+            static ranging_data_t *d;
+            static dw1000_rxpwr_t rxpwr;
+
+            d = data;
+            dw1000_rxpwr(&rxpwr, &d->rxdiag, dw1000_get_current_cfg());
+            printf("SUCCESS %d bias %d fppwr %d rxpwr %d cifo %d\n",
+                (int)(100*d->raw_distance), (int)(100*d->distance),
+                (int)(1000*rxpwr.fp_pwr), (int)(1000*rxpwr.rx_pwr),
+                (int)(10e8*d->freq_offset));
+#if ACQUIRE_CIR
+            uint16_t cir_start = cir_buf[0];
+            uint16_t cir_fp_int = d->rxdiag.firstPath >> 6;
+            uint16_t cir_fp_frac = d->rxdiag.firstPath & 0x3f;
+            // fp (first path) is printed as the integer part and the 1/64 fractional part
+            // "fp a#b" means (a + b/64)
+            printf("CIR [%lu] %02x%02x->%02x%02x %d#%d [%d:%d] ", 
+                seqn,
+                linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
+                dst.u8[0], dst.u8[1],
+                cir_fp_int, cir_fp_frac, cir_start, d->cir_samples_acquired);
+            dw1000_print_cir_hex(cir_buf+1, d->cir_samples_acquired);
+            if (CIR_DUMP_DELAY != 0) {
+              etimer_set(&et_slot, CIR_DUMP_DELAY);
+              PROCESS_WAIT_UNTIL(etimer_expired(&et_slot));
+            }
+#endif
+#if PRINT_RXDIAG
+            printf("DIAG [%lu] %02x%02x->%02x%02x ", 
+                seqn,
+                linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
+                dst.u8[0], dst.u8[1]);
+            print_rxdiag(&d->rxdiag, &rxpwr);
+#endif
+          }
+          else {
+            printf("FAIL\n");
+          }
+        }
+
+        if (RANGING_GAP > 0) {
+          // add a gap between two rangings
+          etimer_set(&et_slot, RANGING_GAP);
+          PROCESS_WAIT_UNTIL(etimer_expired(&et_slot));
         }
       }
-    }
 
-    if (clock_time() - slot_start > TAG_SLOT_DURATION) {
-      printf("Error: tag slot duration exceeded, adjust the timing to avoid collisions!\n");
+      if (clock_time() - slot_start > TAG_SLOT_DURATION) {
+        printf("Error: tag slot duration exceeded, adjust the timing to avoid collisions!\n");
+      }
+      busy_ranging = 0;
     }
   }
 
@@ -333,8 +352,12 @@ bc_recv(struct broadcast_conn *c, const linkaddr_t *from)
     linkaddr_t ordered_tags[num_ordered_tags];
     memcpy(&ordered_tags, &bc_data[9], num_ordered_tags*sizeof(linkaddr_t));
     PRINTF("Control received (seqn: %lu, dur: %lu tags: #%u >", seqn, max_duration, num_ordered_tags);
-    int i;
-    for(i=0; i<num_ordered_tags; i++) {
+    if (busy_ranging) {
+      printf("Error: received a new command while busy with the previous one, adjust the timing to avoid collisions!\n");
+      return;
+    }
+
+    for(int i=0; i<num_ordered_tags; i++) {
       linkaddr_t* tag = &ordered_tags[i];
       PRINTF("%02x%02x ", tag->u8[0], tag->u8[1]);
       if(linkaddr_cmp(&linkaddr_node_addr, tag)) {
@@ -350,7 +373,6 @@ bc_recv(struct broadcast_conn *c, const linkaddr_t *from)
     }
 
     if (found_myself) {
-      PRINTF("Prepare ranging [%lu] (slot: %u)\n", seqn, tag_slot);
       process_post(&ranging_process, master_control_event, NULL);
     }
   }
@@ -363,9 +385,9 @@ static void
 bc_sent(struct broadcast_conn *c, int status, int num_tx) {
   if(DEBUG) {
     if (status == MAC_TX_OK)
-      PRINTF("Control TX ok [%lums]\n", clock_time() * 1000UL / CLOCK_SECOND);
+      PRINTF("Control TX ok %d [%lums]\n", num_tx, clock_time() * 1000UL / CLOCK_SECOND);
     else
-      PRINTF("Control TX failed\n");
+      PRINTF("Control TX failed %d\n", num_tx);
   }
   process_poll(&ranging_process);
 }
